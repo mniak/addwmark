dd0d8c6646cf2ae6f90151320c2b7b6e83535de8
Andre Soares <andre@mniak.net>
2018-04-29 00:41:29 -0300

    Start testi with opencv

--- src/addwmark.cpp
+++ src/addwmark.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 
 using namespace std;
-
+using namespace cv;
 
 int main(int argc, char** argv) {
 
-    cout << "Hello world";
+    Mat image = imread("../test/logo.png", CV_LOAD_IMAGE_COLOR);
     return 0;
 }
